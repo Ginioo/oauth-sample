b07bed6e14a5dd210fad0bb92ac708de3050e8a5
Gino Wu <gino_wu@trendmicro.com>
2022-04-08 22:36:34 +0800

    fix: add route `GET /callback`

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var callback = require('./callback');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/callback', callback);
 
 module.exports = router;
