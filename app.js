f2f87121c9e1ad70253c4a9c7ba995b4b0b9c201
Gino Wu <gino_wu@trendmicro.com>
2022-04-09 09:56:09 +0800

    fix: get access token and save in server session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -11,6 +12,13 @@ var app = express();
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+app.use(session({
+  secret: 'recommand 128 bytes random string',
+  name: '_s', // session id key
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 120 * 1000 } // expired in 2 minutes
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,49 @@
 var express = require('express');
 var qs = require('qs');
 var uuid = require('uuid');
+var axios = require('axios');
 var router = express.Router();
 
-router.get('/login', async (req, res, next) => {
+const redirectUrl = 'http://localhost:3000/callback';
+
+router.get('/subscribe', async (req, res, next) => {
   const params = {
     response_type: 'code',
     client_id: process.env.CLIENT_ID,
-    redirect_uri: 'http://localhost:3000/callback',
+    redirect_uri: redirectUrl,
     scope: 'notify',
     state: uuid.v4(),
   };
   res.redirect(`https://notify-bot.line.me/oauth/authorize?${qs.stringify(params)}`);
 });
 
-router.get('/logout', function (req, res, next) {
-  res.redirect('/');
-});
-
-router.get('/callback', function (req, res, next) {
+router.get('/callback', async function (req, res, next) {
   const { code } = req.query;
-  res.render('callback.ejs', { title: 'Broadcaster', code });
+  const data = {
+    grant_type: 'authorization_code',
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    code,
+    redirect_uri: redirectUrl,
+  };
+  const response = await axios({
+    method: 'POST',
+    url: 'https://notify-bot.line.me/oauth/token',
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    data: qs.stringify(data),
+  });
+  const { access_token: token } = response.data;
+  req.session.token = token;
+  res.render('callback.ejs', { title: 'Broadcaster', btnText: 'Back', code, token });
 });
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index.ejs', { title: 'Broadcaster' });
+  const accessToken = req.session.token;
+  console.log('TEST', req.session);
+  const btnText = accessToken ? 'Subscribed' : 'Subscribe';
+
+  res.render('index.ejs', { title: 'Broadcaster', btnText, isSubscribed: !!accessToken });
 });
 
 module.exports = router;
